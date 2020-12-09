a4cc4a599f9df27a9d7d7328a4e9c5ed9483b5cc
ESTELLE LANDI <estelle.landi@equensworldline.com>
2020-12-09 11:40:33 +0100

    Appcenter-cli

--- step.sh
+++ step.sh
@@ -63,7 +63,9 @@ validate_required_input "appcenter_api_token" ${appcenter_api_token:-}
 validate_required_input "appcenter_name" ${appcenter_name:-}
 validate_required_input "appcenter_org" ${appcenter_org:-}
 validate_required_input "artifact_path" ${artifact_path:-}
+validate_required_input "distribution_groups" ${distribution_groups:-}
 
+#APPCENTER_ACCESS_TOKEN=${appcenter_api_token}
 RELEASE_NOTES_ENCODED="$( jq --null-input --compact-output --arg str "${release_notes:-}" '$str' )"
 
 if [ ! -f "${artifact_path}" ]; then
@@ -76,132 +78,7 @@ then
 	exit 0
 fi
 
-echo_info "Getting a release upload url for ${appcenter_org}/${appcenter_name}"
-TMPFILE=$(mktemp)
-STATUSCODE=$(curl \
-	-X POST \
-	--header 'Content-Type: application/json' \
-	--header "accept: application/json" \
-	--header "X-API-Token: ${appcenter_api_token}" \
-	--silent --show-error \
-	--output /dev/stderr --write-out "%{http_code}" \
-	"https://api.appcenter.ms/v0.1/apps/${appcenter_org}/${appcenter_name}/uploads/releases" \
-	2> "${TMPFILE}")
-if [ "${STATUSCODE}" -ne "201" ]
-then
-	echo_fail "API call failed with ${STATUSCODE}: $(cat ${TMPFILE})"
-fi
-
-UPLOAD_URL=$(cat "${TMPFILE}" | jq .upload_domain --raw-output)
-UPLOAD_ID=$(cat "${TMPFILE}" | jq .id --raw-output)
-
-echo_details "result is ${STATUSCODE}: $(cat ${TMPFILE})"
-rm "${TMPFILE}"
-
-echo_info "Uploading ${artifact_path} to ${UPLOAD_URL}"
-TMPFILE=$(mktemp)
-STATUSCODE=$(curl \
-	-F "ipa=@${artifact_path}" \
-	--silent --show-error \
-	--output /dev/stderr --write-out "%{http_code}" \
-	"${UPLOAD_URL}" \
-	2> "${TMPFILE}")
-if [ "${STATUSCODE}" -gt "299" ]
-then
-	echo_fail "API call failed with ${STATUSCODE}: $(cat ${TMPFILE})"
-fi
-echo_details "result is ${STATUSCODE}: $(cat ${TMPFILE})"
-rm "${TMPFILE}"
-
-echo_info "Committing the release"
-TMPFILE=$(mktemp)
-STATUSCODE=$(curl \
-	-X PATCH \
-	--header "accept: application/json" \
-	--header "X-API-Token: ${appcenter_api_token}" \
-	--header "Content-Type: application/json" \
-	--silent --show-error \
-	--output /dev/stderr --write-out "%{http_code}" \
-	-d '{ "status": "committed"}' \
-	"https://api.appcenter.ms/v0.1/apps/${appcenter_org}/${appcenter_name}/uploads/releases/${UPLOAD_ID}" \
-	2> "${TMPFILE}")
-if [ "${STATUSCODE}" -ne "200" ]
-then
-	echo_fail "API call failed with ${STATUSCODE}: $(cat ${TMPFILE})"
-fi
-RELEASE_ID=$(cat "${TMPFILE}" | jq .id --raw-output)
-echo_details "result is ${STATUSCODE}: $(cat ${TMPFILE})"
-rm "${TMPFILE}"
-
-IFS=', ' read -r -a DISTRIBUTION_GROUPS <<< ${distribution_groups:-}
-if [ ${#DISTRIBUTION_GROUPS[@]} -eq 0 ]
-then
-echo_info "Retrieving distribution groups for ${appcenter_name}"
-TMPFILE=$(mktemp)
-STATUSCODE=$(curl \
-	-X GET \
-	-H "accept: application/json" \
-	-H "X-API-Token: ${appcenter_api_token}" \
-	--silent --show-error \
-	--output /dev/stderr --write-out "%{http_code}" \
-	"https://api.appcenter.ms/v0.1/apps/distribution_groups/${appcenter_org}/${appcenter_name}" \
-	2> ${TMPFILE})
-if [ "${STATUSCODE}" -ne "200" ]
-then
-	echo_fail "API call failed with ${STATUSCODE}: $(cat ${TMPFILE})"
-fi
-echo_details "result is ${STATUSCODE}: $(cat ${TMPFILE})"
-SAVEIFS="${IFS}"
-IFS=$'\n'
-DISTRIBUTION_GROUPS=( $(cat "${TMPFILE}" | jq '.[].name' --raw-output) )
-IFS="${SAVEIFS}"
-rm "${TMPFILE}"
-fi
-echo_details "distribution groups are ${DISTRIBUTION_GROUPS[*]}"
-
-for DISTRIBUTION_GROUP in "${DISTRIBUTION_GROUPS[@]}"
-do
-	DISTRIBUTION_GROUP_ENCODED="$( jq --null-input --compact-output --arg str "$DISTRIBUTION_GROUP" '$str' )"
-	echo_info "Adding to distribution group ${DISTRIBUTION_GROUP}"
-	TMPFILE=$(mktemp)
-	STATUSCODE=$(curl -X PATCH \
-		--header "Content-Type: application/json" \
-		--header "Accept: application/json" \
-		--header "X-API-Token: ${appcenter_api_token}" \
-		--silent --show-error \
-		--output /dev/stderr --write-out "%{http_code}" \
-		-d "{ \"destination_name\": ${DISTRIBUTION_GROUP_ENCODED}, \"release_notes\": ${RELEASE_NOTES_ENCODED}, \"mandatory_update\": ${mandatory_update:-false}, \"notify_testers\": ${notify_testers:-true}}" \
-		"https://api.appcenter.ms/v0.1/apps/${appcenter_org}/${appcenter_name}/releases/${RELEASE_ID}" \
-		2> "${TMPFILE}")
-
-	if [ "${STATUSCODE}" -ne "200" ]
-	then
-		echo_fail "API call failed with ${STATUSCODE}: $(cat ${TMPFILE})"
-	fi
-	echo_details "result is ${STATUSCODE}: $(cat ${TMPFILE})"
-	rm "${TMPFILE}"
-done
-
-echo_info "Retrieving download url"
-TMPFILE=$(mktemp)
-STATUSCODE=$(curl -X GET \
-	--header "Accept: application/json" \
-	--header "X-API-Token: ${appcenter_api_token}" \
-	--silent --show-error \
-	--output /dev/stderr --write-out "%{http_code}" \
-	"https://api.appcenter.ms/v0.1/apps/${appcenter_org}/${appcenter_name}/releases/${RELEASE_ID}" \
-	2> "${TMPFILE}")
-
-if [ "${STATUSCODE}" -ne "200" ]
-then
-	echo_fail "API call failed with ${STATUSCODE}: $(cat ${TMPFILE})"
-fi
-DOWNLOAD_URL=$(cat "${TMPFILE}" | jq .download_url --raw-output)
-
-echo_details "result is ${STATUSCODE}: $(cat ${TMPFILE})"
-echo_details "APPCENTER_DOWNLOAD_URL is ${DOWNLOAD_URL}"
-envman add --key APPCENTER_DOWNLOAD_URL --value "${DOWNLOAD_URL}"
-
-rm "${TMPFILE}"
-
+npm install appcenter-cli
+appcenter login --token ${appcenter_api_token} --debug
+appcenter distribute release --app "${appcenter_org}/${appcenter_name}" --file "${artifact_path}" --group "${distribution_groups}" --release-notes "${release_notes:-}" --debug
 echo_done "Completed AppCenter app upload at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
